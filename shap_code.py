c20deed2a9cd0db5cad54422e45652ab38f5e63f
Learning <88524372+CurryevtWSN@users.noreply.github.com>
2023-08-25 17:47:19 +0800

    Update shap_code.py

--- shap_code.py
+++ shap_code.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import scikit-learn
+import sklearn
 import matplotlib.pyplot as plt
 from sklearn.ensemble import GradientBoostingClassifier
 from imblearn.over_sampling import RandomOverSampler
